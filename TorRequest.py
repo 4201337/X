e52f0388670f79be86273712c3e06c09e76dec28
4201337 <4201337@users.noreply.github.com>
2016-09-04 19:17:51 +0000

    Update TorRequest.py

--- TorRequest.py
+++ TorRequest.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 import requesocks
-import requests
+
 session = requesocks.session()
-session.proxies = {'http': 'socks5://127.0.0.1:9150', 'https': 'socks5://127.0.0.1:9150'}
-print session.get("http://httpbin.org/ip").text
-print requests.get("http://httpbin.org/ip").text
+session.proxies = {
+    'http': 'socks5://127.0.0.1:9150',
+    'https': 'socks5://127.0.0.1:9150'
+}
+
+print session.get("https://wtfismyip.com/text").text
