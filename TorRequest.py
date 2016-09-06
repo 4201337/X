53a954c49ba572139d07f6b41362087048e42a9a
4201337 <4201337@users.noreply.github.com>
2016-09-06 08:06:16 +0000

    Update TorRequest.py

--- TorRequest.py
+++ TorRequest.py
@@ -2,10 +2,28 @@
 
 import requesocks
 
-session = requesocks.session()
-session.proxies = {
-    'http': 'socks5://127.0.0.1:9150',
-    'https': 'socks5://127.0.0.1:9150'
-}
-
-print session.get("https://wtfismyip.com/text").text
+with requesocks.session() as req:
+    
+    req.proxies = {
+        'http': 'socks5://127.0.0.1:9150',
+        'https': 'socks5://127.0.0.1:9150'
+    }
+    
+    req.headers = {
+        'Accept': 'No',
+        'Accept-Encoding': 'No',
+        'Referer': 'http://1.3.3.7/',
+        'User-Agent': '1337',
+    }
+    
+    req.cert = ''
+    
+    req.stream = True
+    
+    try:
+        url = req.get('https://httpbin.org/ip')
+        print url.text
+        url = req.get('https://httpbin.org/headers')
+        print url.text
+    except requesocks.exceptions.RequestException, e:
+        print e
